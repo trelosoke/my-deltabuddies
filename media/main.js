d8e7edc50dc0e69c601eb8e5d00c4cbf618663a9
Treloso <diogo.trelosocnt@gmail.com>
2026-07-14 11:45:18 -0300

    feat(webview): implement spritesheet lines for different animation drawing

--- media/main.js
+++ media/main.js
@@ -43,25 +43,32 @@ canvas.width = document.body.clientWidth;
 canvas.height = 200;
 
 const totalFrames = 4;
+const totalLines = 4;
 const frameWidth = 32;
 const frameHeight = 48;
 
 let currentFrame = 0;
 let frameCounter = 0;
+let animationLine = 0;
 const FRAMES_PER_SPRITE = 10;
 
 
 function animateKrisWalk() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
+    // Adds conditional nesting for testing purposes
     if (frameCounter % FRAMES_PER_SPRITE === 0) {
         currentFrame = (currentFrame + 1) % totalFrames;
+        
+        if (currentFrame === 0) {
+            animationLine = (animationLine + 1) % totalLines;
+        }
     }
 
     const scale = 2.5;
 
     let widthUpscale = frameWidth * scale;
-    let heightUpscale = frameHeight * scale; 
+    let heightUpscale = frameHeight * scale;
 
     ctx.fillStyle = 'lightblue';
     ctx.fillRect(0, 0, canvas.width, canvas.height);
@@ -69,7 +76,7 @@ function animateKrisWalk() {
     ctx.imageSmoothingEnabled = false;
     ctx.drawImage(
         krisWalk,
-        currentFrame * frameWidth, 0,
+        currentFrame * frameWidth, animationLine * frameHeight,
         frameWidth, frameHeight,
         0, 0,
         widthUpscale, heightUpscale
