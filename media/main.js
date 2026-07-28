662f8ab9b49f2d398a6cf3bf8ee32ccb37cc0fc5
Treloso <diogo.trelosocnt@gmail.com>
2026-07-28 09:32:19 -0300

    feat(characters): implement config-driven multi-character loading from charactersConfig

--- media/main.js
+++ media/main.js
@@ -1,3 +1,7 @@
+import { createCharacter } from './characters.js';
+import { charactersConfig } from './charactersConfig.js';
+import { startAnimation } from './animation.js';
+
 /** @type {HTMLCanvasElement} */
 const canvas = document.getElementById('aquarium');
 
@@ -14,128 +18,41 @@ const resizeObserver = new ResizeObserver(() => {
 
 resizeObserver.observe(document.body);
 
+const characters = [];
+
 /** @type {{krisUri: string}} */
-const characters = JSON.parse(canvas.dataset.characters);
+const spritesSource = JSON.parse(canvas.dataset.characters);
 
-const krisSpriteSheet = new Image();
-let characterSpriteSheet = krisSpriteSheet;
+const characterData = [
+    {
+        name: 'kris',
+        src: spritesSource.krisUri,
+        config: charactersConfig.kris
+    },
+    {
+        name: 'susie',
+        src: spritesSource.susieUri,
+        config: charactersConfig.susie
+    },
+    {
+        name: 'ralsei',
+        src: spritesSource.ralseiUri,
+        config: charactersConfig.ralsei
+    }
+];
 
-const totalFrames = 4;
-const totalAnimationLines = 4;
-const spriteWidth = 32;
-const spriteHeight = 48;
+characterData.forEach(data => {
+    const img = new Image();
+    
+    img.onload = () => {
+        const character = createCharacter(img, data.config);
+        characters.push(character);
 
-let currentFrame = 0;
-let frameCounter = 1;
-const FRAMES_PER_SPRITE = 14;
-
-const CenterX = canvas.width / 2;
-const CenterY = canvas.height / 2;
-let spriteCenterX;
-let spriteCenterY;
-
-let posX = CenterX, 
-    posY = CenterY;
-let currentDirection = 'down';
-let isIdle = false;
-let idleCounter = 0;
-
-const speed = 0.4;
-const spriteDirectionLine = { down: 0, left: 1, right: 2, up: 3};
-
-function updateMovement() {
-    const movements = {
-        right: () => { posX += speed; },
-        left: () => { posX -= speed; },
-        up: () => { posY -= speed; },
-        down: () => { posY += speed; }
+        console.log(characters);
+        if (characters.length === characterData.length) {
+            startAnimation(canvas, ctx, characters);
+        }
     };
 
-    movements[currentDirection]();
-}
-
-function bounceMovement(pos, spriteCenter, min, max, [towardMin, towardMax]) {
-    if (pos + spriteCenter + speed > max) {
-        return towardMin;
-    } else if (pos - speed < min) {
-        return towardMax;
-    }
-    return null;
-}
-
-let idleDuration = Math.floor(Math.random() * (260 - 90) + 90);
-let animationIdleInterval = Math.floor(Math.random() * (720 - 300) + 300);
-let directionTrigger = Math.floor(Math.random() * (260 - 80) + 80);
-
-const scale = 2;
-let spriteWidthUpscale = spriteWidth * scale;
-let spriteHeightUpscale = spriteHeight * scale;
-
-let newDirectionX;
-let newDirectionY;
-
-function animate() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-    spriteCenterX = spriteWidthUpscale / 2;
-    spriteCenterY = spriteHeightUpscale / 2;
-
-    ctx.imageSmoothingEnabled = false;
-    ctx.drawImage(
-        characterSpriteSheet,
-        currentFrame * spriteWidth, spriteDirectionLine[currentDirection] * spriteHeight,
-        spriteWidth, spriteHeight,
-        posX - spriteCenterX, posY - spriteCenterY,
-        spriteWidthUpscale, spriteHeightUpscale
-    );
-
-    if (!isIdle) {
-        if (frameCounter % animationIdleInterval === 0){
-            animationIdleInterval = Math.floor(Math.random() * (720 - 300) + 300);
-            isIdle = true;
-            idleCounter = 0;
-        }
-
-        if (frameCounter % FRAMES_PER_SPRITE === 0) {
-            currentFrame = (currentFrame + 1) % totalFrames;
-        }
-
-        const directions = ['right', 'left', 'up', 'down'];
-        if (frameCounter % directionTrigger === 0) {
-            directionTrigger = Math.floor(Math.random() * (260 - 80) + 80);
-            currentDirection = directions[Math.floor(Math.random() * 4)];
-        }
-
-        newDirectionX = bounceMovement(posX, spriteCenterX, spriteCenterX, canvas.width, ['left', 'right']);
-        newDirectionY = bounceMovement(posY, spriteCenterY, spriteCenterY, canvas.height, ['up', 'down']);
-
-        if (newDirectionX) {
-            currentDirection = newDirectionX;
-        } else if (newDirectionY) {
-            currentDirection = newDirectionY;
-        }
-
-        updateMovement();
-    }
-
-    if (isIdle) {
-        if (idleCounter >= idleDuration) {
-            idleDuration = Math.floor(Math.random() * (260 - 90) + 90);
-            isIdle = false;
-            idleCounter = 0;
-        } else {
-            ++idleCounter;
-            currentFrame = 0;
-        }
-    }
-
-    ++frameCounter;
-
-    requestAnimationFrame(animate);
-}
-
-krisSpriteSheet.onload = () => {
-    animate(krisSpriteSheet);
-};
-
-krisSpriteSheet.src = characters.krisUri;
+    img.src = data.src;
+});
