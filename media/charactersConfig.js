08b742865cb284ff74ac1c71ddfd0b51a016e312
Treloso <diogo.trelosocnt@gmail.com>
2026-08-18 10:09:11 -0300

    feat(characters-config): add idle actions for all characters
    - Add hold_flower idle action to kris
    
    - Add eat_chalk, laugh, yawning and stretching idle actions to susie
    
    - Add sit idle action to ralsei

--- media/charactersConfig.js
+++ media/charactersConfig.js
@@ -36,6 +36,18 @@ export const charactersConfig = {
                 sustainSeconds: 6,
                 chance: 90,
                 allowedDirections: ['down']
+            },
+
+            hold_flower: {
+                type: 'action',
+                frameDelay: 16,
+                startRow: 5,
+                rowCount: 1,
+                spritesPerRow: 16,
+                directionMode: 'fixed',
+                sustainSeconds: 0,
+                chance: 99,
+                allowedDirections: ['down']
             }
         },
         
@@ -79,6 +91,66 @@ export const charactersConfig = {
                 spritesPerRow: 4,
                 directionMode: '4way',
                 directionOrder: ['down', 'left', 'right', 'up']
+            },
+
+            eat_chalk_right: {
+                type: 'action',
+                frameDelay: 7.5,
+                startRow: 4,
+                rowCount: 1,
+                spritesPerRow: 22,
+                directionMode: 'fixed',
+                sustainSeconds: 0,
+                chance: 20,
+                allowedDirections: ['right']
+            },
+
+            eat_chalk_left: {
+                type: 'action',
+                frameDelay: 7.5,
+                startRow: 5,
+                rowCount: 1,
+                spritesPerRow: 22,
+                directionMode: 'fixed',
+                sustainSeconds: 0,
+                chance: 99,
+                allowedDirections: ['left']
+            },
+
+            laugh: {
+                type: 'action',
+                frameDelay: 14,
+                startRow: 6,
+                rowCount: 1,
+                spritesPerRow: 10,
+                directionMode: 'fixed',
+                sustainSeconds: 0,
+                chance: 42,
+                allowedDirections: ['down', 'right']
+            },
+
+            yawning: {
+                type: 'action',
+                frameDelay: 18,
+                startRow: 7,
+                rowCount: 1,
+                spritesPerRow: 10,
+                directionMode: 'fixed',
+                sustainSeconds: 0,
+                chance: 99,
+                allowedDirections: ['down']
+            },
+
+            stretching: {
+                type: 'action',
+                frameDelay: 10,
+                startRow: 8,
+                rowCount: 1,
+                spritesPerRow: 9,
+                directionMode: 'fixed',
+                sustainSeconds: 2.5,
+                chance: 38,
+                allowedDirections: ['down']
             }
         },
 
@@ -122,6 +194,18 @@ export const charactersConfig = {
                 spritesPerRow: 4,
                 directionMode: '4way',
                 directionOrder: ['down', 'left', 'right', 'up']
+            },
+
+            sit: {
+                type: 'action',
+                frameDelay: 12,
+                startRow: 4,
+                rowCount: 1,
+                spritesPerRow: 10,
+                directionMode: 'fixed',
+                sustainSeconds: 2.5,
+                chance: 99,
+                allowedDirections: ['down', 'left']
             }
         },
 
